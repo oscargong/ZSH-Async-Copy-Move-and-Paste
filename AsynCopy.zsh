870facda88355f9c59a1475184de5f515f629100
Oscar Gong <oscargong1995@gmail.com>
2021-06-15 16:03:25 -0700

    Add files via upload

---
+++ AsynCopy.zsh
@@ -0,0 +1,26 @@
+# async copy
+function ac() {
+    unset ACOPY
+    unset AMOVE
+    export ACOPY=("${(@f)$(realpath -e $@)}")
+}
+
+# async move
+function am() {
+    unset ACOPY
+    unset AMOVE
+    export AMOVE=("${(@f)$(realpath -e $@)}")
+}
+
+# async paste
+function ap() {
+    if [ ! -z "$ACOPY" ]; then
+        cp -vr "${ACOPY[@]}" .
+    elif [ ! -z "$AMOVE" ]; then
+        mv -v "${AMOVE[@]}" .
+        unset AMOVE
+    else
+        >&2 echo "clipboard empty"
+        return 1
+    fi
+}
